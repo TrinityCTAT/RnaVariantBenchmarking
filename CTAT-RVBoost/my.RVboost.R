2e3fea7edda3e54df57d3993b1b6cf9f72ef97f2
Brian Haas <brianjohnhaas@gmail.com>
2019-09-15 11:59:55 -0400

    bugfix

--- CTAT-RVBoost/my.RVboost.R
+++ CTAT-RVBoost/my.RVboost.R
@@ -14,7 +14,7 @@ inputVCF <- stdin[1]
 sel.attri <- c("DJ","PctExtPos","ReadPosRankSum","QD","FS","ED") 
 
 if (length(stdin) == 2) {
-    sel.attri = strsplit(stdin[2], ",")
+    sel.attri = strsplit(stdin[2], ",")[[1]]
 }
 message("Using attribute list: ", sel.attri)
 
@@ -37,14 +37,19 @@ library(gbm)
 library(mgcv)
 
 
-parseVCF <- function( VCF.filename, genome.ver = "hg19", 
-                      sel.info.attr = c("QD","ReadPosRankSum","DP","FS","MQ"),
-                      with.fliter = TRUE)
+parseVCF <- function(VCF.filename,
+                     genome.ver = "hg19", 
+                     sel.info.attr = c("QD","ReadPosRankSum","DP","FS","MQ"),
+                     with.fliter = TRUE)
 {
-  
+
+    message("parsing vcf file:", VCF.filename)
     tmp.vcf <- suppressWarnings(readVcf(file=VCF.filename,genome=genome.ver))
     
     info.res <- as.data.frame(info(tmp.vcf))
+
+    message("Existing and available attributes include: ")
+    print(colnames(info.res))
     
     check.flag <- is.element(sel.info.attr,colnames(info.res))
     if(!all(check.flag))
@@ -75,7 +80,7 @@ parseVCF <- function( VCF.filename, genome.ver = "hg19",
     }
     
     ret.res$GT.vec <- geno(tmp.vcf)$GT
-                                        #ret.res$pos.vec <- paste(rowData(tmp.vcf)@seqnames, start(rowData(tmp.vcf)@ranges),sep=":")
+    ##ret.res$pos.vec <- paste(rowData(tmp.vcf)@seqnames, start(rowData(tmp.vcf)@ranges),sep=":")
     ret.res$pos.vec <- paste(seqnames(tmp.vcf), start(ranges(tmp.vcf)),sep=":")  # from vadir
     
     return(ret.res)
@@ -85,7 +90,8 @@ parseVCF <- function( VCF.filename, genome.ver = "hg19",
 fitRVmodel <- function( input.mtx, DB.filename, DB.ID="rsid", pos.vec=NULL,
                        model.type="adaboost", ada.n=5e3)
 {
-    
+
+    message("Fitting RV model")
     model.type <- tolower(model.type)
     
     provided.model.type.vec <- 
